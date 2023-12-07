4e44a9110577d521bfb2108741f38c90d03adb2f
Eliza Moritz <dark.little.finch@gmail.com>
2023-12-08 00:40:03 +0700

    feat: user model

---
+++ app/src/main/java/hexlet/code/app/model/User.java
@@ -0,0 +1,54 @@
+package hexlet.code.app.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@ToString(onlyExplicitlyIncluded = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ToString.Include
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @NotBlank
+    @ToString.Include
+    private String firstName;
+
+    @NotBlank
+    @ToString.Include
+    private String lastName;
+
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    private String passwordDigest;
+
+    @CreatedDate
+    private LocalDate createdAt;
+
+    @LastModifiedDate
+    private LocalDate updatedAt;
+}
