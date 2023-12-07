40c048b654a9810633f6c74002b0a8f57dda75e2
Eliza Moritz <dark.little.finch@gmail.com>
2023-12-07 19:13:31 +0700

    feat: initial project setup

---
+++ app/build.gradle.kts
@@ -0,0 +1,82 @@
+import com.adarshr.gradle.testlogger.theme.ThemeType
+
+plugins {
+	application
+	checkstyle
+	jacoco
+	id("org.springframework.boot") version "3.2.0"
+	id("io.spring.dependency-management") version "1.1.4"
+	id("se.patrikerdes.use-latest-versions") version "0.2.18"
+	id("com.github.ben-manes.versions") version "0.47.0"
+	id("com.adarshr.test-logger") version "4.0.0"
+}
+
+group = "hexlet.code"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_20
+}
+
+application {
+	mainClass = "hexlet.code.app.AppApplication"
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["snippetsDir"] = file("build/generated-snippets")
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+
+//	implementation("org.springframework.boot:spring-boot-starter-security")
+//	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+	implementation("org.mapstruct:mapstruct:1.5.5.Final")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+
+	runtimeOnly("com.h2database:h2")
+	runtimeOnly("org.postgresql:postgresql")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+//	testImplementation("org.springframework.security:spring-security-test")
+	testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
+	implementation("net.datafaker:datafaker:2.0.1")
+	implementation("org.instancio:instancio-junit:3.3.0")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.test {
+	useJUnitPlatform()
+	finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+	dependsOn(tasks.test)
+}
+
+testlogger {
+	theme = ThemeType.MOCHA
+	showStandardStreams = true
+}
---
+++ app/src/main/java/hexlet/code/app/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package hexlet.code.app.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/welcome")
+public class WelcomeController {
+
+    @GetMapping(path = "")
+    public String welcome() {
+        return "Welcome to Spring!";
+    }
+}
