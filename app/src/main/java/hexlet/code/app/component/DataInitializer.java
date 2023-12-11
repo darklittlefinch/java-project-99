511843739d18e99af8f3011ca5c150e4906f7213
Eliza Moritz <dark.little.finch@gmail.com>
2023-12-12 04:19:26 +0700

    add: admin

---
+++ app/src/main/java/hexlet/code/app/component/DataInitializer.java
@@ -0,0 +1,33 @@
+package hexlet.code.app.component;
+
+import hexlet.code.app.model.User;
+import hexlet.code.app.repository.UserRepository;
+import hexlet.code.app.service.CustomUserDetailsService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class DataInitializer implements ApplicationRunner {
+
+    private final String ADMIN_EMAIL = "hexlet@example.com";
+    private final String ADMIN_PASSWORD = "qwerty";
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private CustomUserDetailsService userService;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        var admin = new User();
+        admin.setEmail(ADMIN_EMAIL);
+        admin.setPasswordDigest(ADMIN_PASSWORD);
+
+        userService.createUser(admin);
+    }
+}
