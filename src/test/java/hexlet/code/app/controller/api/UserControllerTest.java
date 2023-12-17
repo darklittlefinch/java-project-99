f39e8f41676194ab12cc5d0b11fa42e6916a0635
Eliza Moritz <dark.little.finch@gmail.com>
2023-12-18 04:28:44 +0700

    style: useless import was deleted

--- src/test/java/hexlet/code/app/controller/api/UserControllerTest.java
+++ src/test/java/hexlet/code/app/controller/api/UserControllerTest.java
@@ -16,7 +16,6 @@ import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequ
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hexlet.code.app.repository.UserRepository;
 import hexlet.code.app.util.TestUtils;
-import net.datafaker.Faker;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
