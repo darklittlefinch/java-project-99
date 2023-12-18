1641cd33b1fb9ee1f7785dcf2762c2367ff965f9
Eliza Moritz <dark.little.finch@gmail.com>
2023-12-18 07:40:05 +0700

    add: default task statuses

--- src/main/java/hexlet/code/app/component/DataInitializer.java
+++ src/main/java/hexlet/code/app/component/DataInitializer.java
@@ -1,8 +1,9 @@
 package hexlet.code.app.component;
 
-import hexlet.code.app.model.User;
+import hexlet.code.app.repository.TaskStatusRepository;
 import hexlet.code.app.repository.UserRepository;
 import hexlet.code.app.service.CustomUserDetailsService;
+import hexlet.code.app.util.TaskStatusUtils;
 import hexlet.code.app.util.UserUtils;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,17 +16,39 @@ import org.springframework.stereotype.Component;
 public class DataInitializer implements ApplicationRunner {
 
     @Autowired
-    private UserRepository userRepository;
+    private TaskStatusRepository taskStatusRepository;
 
     @Autowired
     private CustomUserDetailsService userService;
 
+    @Autowired
+    private UserUtils userUtils;
+
+    @Autowired
+    private TaskStatusUtils taskStatusUtils;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        var admin = new User();
-        admin.setEmail(UserUtils.ADMIN_EMAIL);
-        admin.setPassword(UserUtils.ADMIN_PASSWORD);
-
+        var admin = userUtils.getAdmin();
         userService.createUser(admin);
+
+        var draftStatus = taskStatusUtils.getTaskStatus(TaskStatusUtils.DRAFT_NAME, TaskStatusUtils.DRAFT_SLUG);
+        taskStatusRepository.save(draftStatus);
+
+        var toReviewStatus = taskStatusUtils.getTaskStatus(TaskStatusUtils.TO_REVIEW_NAME,
+                TaskStatusUtils.TO_REVIEW_SLUG);
+        taskStatusRepository.save(toReviewStatus);
+
+        var toBeFixedStatus = taskStatusUtils.getTaskStatus(TaskStatusUtils.TO_BE_FIXED_NAME,
+                TaskStatusUtils.TO_BE_FIXED_SLUG);
+        taskStatusRepository.save(toBeFixedStatus);
+
+        var toPublishStatus = taskStatusUtils.getTaskStatus(TaskStatusUtils.TO_PUBLISH_NAME,
+                TaskStatusUtils.TO_PUBLISH_SLUG);
+        taskStatusRepository.save(toPublishStatus);
+
+        var publishedStatus = taskStatusUtils.getTaskStatus(TaskStatusUtils.PUBLISHED_NAME,
+                TaskStatusUtils.PUBLISHED_SLUG);
+        taskStatusRepository.save(publishedStatus);
     }
 }
