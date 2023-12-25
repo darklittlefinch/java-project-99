191f914166bb777ff3cd0d1f6d397043219ed48c
Eliza Moritz <dark.little.finch@gmail.com>
2023-12-25 18:25:59 +0700

    feat: toEntity method impl for Task model

--- src/main/java/hexlet/code/app/mapper/ReferenceMapper.java
+++ src/main/java/hexlet/code/app/mapper/ReferenceMapper.java
@@ -1,6 +1,8 @@
 package hexlet.code.app.mapper;
 
 import hexlet.code.app.model.BaseEntity;
+import hexlet.code.app.model.TaskStatus;
+import hexlet.code.app.repository.TaskStatusRepository;
 import jakarta.persistence.EntityManager;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
@@ -14,7 +16,15 @@ public abstract class ReferenceMapper {
     @Autowired
     private EntityManager entityManager;
 
+    @Autowired
+    private TaskStatusRepository taskStatusRepository;
+
     public <T extends BaseEntity> T toEntity(Long id, @TargetType Class<T> entityClass) {
         return id != null ? entityManager.find(entityClass, id) : null;
+    }
+
+    public TaskStatus toEntity(String slug) {
+        var taskStatusOptional = taskStatusRepository.findBySlug(slug);
+        return taskStatusOptional.orElse(null);
     }
 }
