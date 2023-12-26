2766d4dd4f84ab5c04a72c0f838c433478f04755
Eliza Moritz <dark.little.finch@gmail.com>
2023-12-26 22:53:21 +0700

    style: names of statuses were changed

--- src/main/java/hexlet/code/app/util/TaskStatusUtils.java
+++ src/main/java/hexlet/code/app/util/TaskStatusUtils.java
@@ -8,14 +8,13 @@ import java.util.List;
 
 @Component
 public class TaskStatusUtils {
-
     @Bean
     public List<TaskStatus> getDefaultTaskStatuses() {
-        var draftStatus = getTaskStatus("Draft...", "draft");
-        var toReviewStatus = getTaskStatus("To review...", "to_review");
-        var toBeFixedStatus = getTaskStatus("To be fixed...", "to_be_fixed");
-        var toPublishStatus = getTaskStatus("To publish...", "to_publish");
-        var publishedStatus = getTaskStatus("Published!", "published");
+        var draftStatus = getTaskStatus("Draft", "draft");
+        var toReviewStatus = getTaskStatus("To review", "to_review");
+        var toBeFixedStatus = getTaskStatus("To be fixed", "to_be_fixed");
+        var toPublishStatus = getTaskStatus("To publish", "to_publish");
+        var publishedStatus = getTaskStatus("Published", "published");
 
         return List.of(
                 draftStatus,
