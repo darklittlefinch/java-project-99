7fdb8465a13bf67ee8efeaeafcba4a2c8b914562
Eliza Moritz <dark.little.finch@gmail.com>
2023-12-26 20:31:58 +0700

    add: labels controller

---
+++ src/main/java/hexlet/code/app/service/LabelService.java
@@ -0,0 +1,52 @@
+package hexlet.code.app.service;
+
+import hexlet.code.app.dto.labelDto.LabelCreateDTO;
+import hexlet.code.app.dto.labelDto.LabelDTO;
+import hexlet.code.app.dto.labelDto.LabelUpdateDTO;
+import hexlet.code.app.exception.ResourceNotFoundException;
+import hexlet.code.app.mapper.LabelMapper;
+import hexlet.code.app.repository.LabelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LabelService {
+
+    @Autowired
+    private LabelRepository labelRepository;
+
+    @Autowired
+    private LabelMapper labelMapper;
+
+    public List<LabelDTO> getAll() {
+        return labelRepository.findAll().stream()
+                .map(labelMapper::map)
+                .toList();
+    }
+
+    public LabelDTO findById(Long id) {
+        var label = labelRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Label not found"));
+        return labelMapper.map(label);
+    }
+
+    public LabelDTO create(LabelCreateDTO dto) {
+        var label = labelMapper.map(dto);
+        labelRepository.save(label);
+        return labelMapper.map(label);
+    }
+
+    public LabelDTO update(Long id, LabelUpdateDTO dto) {
+        var label = labelRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Label not found"));
+        labelMapper.update(dto, label);
+        labelRepository.save(label);
+        return labelMapper.map(label);
+    }
+
+    public void delete(Long id) {
+        labelRepository.deleteById(id);
+    }
+}
