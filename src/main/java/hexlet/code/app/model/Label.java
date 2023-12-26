ee7885b9fc8bdfe2f860735f316e6b826baed367
Eliza Moritz <dark.little.finch@gmail.com>
2023-12-26 19:01:04 +0700

    add: Label model

---
+++ src/main/java/hexlet/code/app/model/Label.java
@@ -0,0 +1,42 @@
+package hexlet.code.app.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "labels")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@ToString(onlyExplicitlyIncluded = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Label implements BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ToString.Include
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(unique = true)
+    @NotBlank
+    @ToString.Include
+    private String name;
+
+    @CreatedDate
+    private Instant createdAt;
+}
