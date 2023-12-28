9045a9f3cacd862dff7f83544354bf4029df533d
Eliza Moritz <dark.little.finch@gmail.com>
2023-12-29 02:22:03 +0700

    test: test for sentry tracker

--- src/main/java/hexlet/code/app/AppApplication.java
+++ src/main/java/hexlet/code/app/AppApplication.java
@@ -5,12 +5,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import java.lang.Exception;
+import io.sentry.Sentry;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class AppApplication {
 
     public static void main(String[] args) {
+
+        try {
+            throw new Exception("This is a test.");
+        } catch (Exception e) {
+            Sentry.captureException(e);
+        }
+
         SpringApplication.run(AppApplication.class, args);
     }
 
