3c88410a0534c5b2aa396e2f8d8aa0969d98c6f1
Eliza Moritz <dark.little.finch@gmail.com>
2023-12-29 03:25:52 +0700

    style: delete unused import

--- src/main/java/hexlet/code/app/AppApplication.java
+++ src/main/java/hexlet/code/app/AppApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import java.lang.Exception;
 import io.sentry.Sentry;
 
 @SpringBootApplication
