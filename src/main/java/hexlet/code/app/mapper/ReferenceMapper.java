76419f1ddacbcc4c0f678ad6227066286f54a7de
Eliza Moritz <dark.little.finch@gmail.com>
2024-01-04 20:59:17 +0700

    refactor: method toEntity for status was moved to TaskMapper

--- src/main/java/hexlet/code/app/mapper/ReferenceMapper.java
+++ src/main/java/hexlet/code/app/mapper/ReferenceMapper.java
@@ -16,15 +16,7 @@ public abstract class ReferenceMapper {
     @Autowired
     private EntityManager entityManager;
 
-    @Autowired
-    private TaskStatusRepository taskStatusRepository;
-
     public <T extends BaseEntity> T toEntity(Long id, @TargetType Class<T> entityClass) {
         return id != null ? entityManager.find(entityClass, id) : null;
-    }
-
-    public TaskStatus toEntity(String slug) {
-        var taskStatusOptional = taskStatusRepository.findBySlug(slug);
-        return taskStatusOptional.orElse(null);
     }
 }
--- src/main/java/hexlet/code/app/mapper/TaskMapper.java
+++ src/main/java/hexlet/code/app/mapper/TaskMapper.java
@@ -3,13 +3,17 @@ package hexlet.code.app.mapper;
 import hexlet.code.app.dto.taskDto.TaskCreateDTO;
 import hexlet.code.app.dto.taskDto.TaskDTO;
 import hexlet.code.app.dto.taskDto.TaskUpdateDTO;
+import hexlet.code.app.exception.ResourceNotFoundException;
 import hexlet.code.app.model.Task;
+import hexlet.code.app.model.TaskStatus;
+import hexlet.code.app.repository.TaskStatusRepository;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(
         uses = {JsonNullableMapper.class, ReferenceMapper.class},
@@ -18,6 +22,9 @@ import org.mapstruct.ReportingPolicy;
         unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
 public abstract class TaskMapper {
+
+    @Autowired
+    private TaskStatusRepository taskStatusRepository;
 
     @Mapping(source = "assigneeId", target = "assignee")
     @Mapping(source = "status", target = "taskStatus")
@@ -36,4 +43,9 @@ public abstract class TaskMapper {
     @Mapping(source = "name", target = "title")
     @Mapping(source = "description", target = "content")
     public abstract TaskDTO map(Task model);
+
+    public TaskStatus toEntity(String slug) {
+        return taskStatusRepository.findBySlug(slug)
+                .orElseThrow(() -> new ResourceNotFoundException("Status not found"));
+    }
 }
