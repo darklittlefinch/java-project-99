2bdaf58e5f261402e16222184695b3fb9403b470
Eliza Moritz <dark.little.finch@gmail.com>
2024-01-10 22:21:40 +0700

    refactor: current user check

--- src/main/java/hexlet/code/controller/api/UserController.java
+++ src/main/java/hexlet/code/controller/api/UserController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+
+    private static final String ONLY_OWNER_BY_ID
+            = "@userRepository.findById(#id).get().getEmail() == authentication.getName()";
 
     @Autowired
     private UserService userService;
@@ -50,6 +54,7 @@ public class UserController {
 
     @PutMapping(path = "/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize(ONLY_OWNER_BY_ID)
     public UserDTO update(
             @Valid @RequestBody UserUpdateDTO dto,
             @PathVariable Long id) {
@@ -58,6 +63,7 @@ public class UserController {
 
     @DeleteMapping(path = "/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize(ONLY_OWNER_BY_ID)
     public void destroy(@PathVariable Long id) {
         userService.delete(id);
     }
--- src/main/java/hexlet/code/service/UserService.java
+++ src/main/java/hexlet/code/service/UserService.java
@@ -3,7 +3,6 @@ package hexlet.code.service;
 import hexlet.code.dto.userDto.UserCreateDTO;
 import hexlet.code.dto.userDto.UserDTO;
 import hexlet.code.dto.userDto.UserUpdateDTO;
-import hexlet.code.exception.AccessDeniedException;
 import hexlet.code.exception.AssociatedWithEntityException;
 import hexlet.code.exception.ResourceNotFoundException;
 import hexlet.code.mapper.UserMapper;
@@ -57,27 +56,20 @@ public class UserService {
         var user = userRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found"));
 
-        var currentUser = userUtils.getCurrentUser();
+        userMapper.update(dto, user);
 
-        if (currentUser == null || !currentUser.getId().equals(id)) {
-            throw new AccessDeniedException("Access denied");
-        } else {
-            userMapper.update(dto, user);
+        var hashedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hashedPassword);
 
-            var hashedPassword = passwordEncoder.encode(user.getPassword());
-            user.setPassword(hashedPassword);
+        userRepository.save(user);
+        return userMapper.map(user);
 
-            userRepository.save(user);
-            return userMapper.map(user);
-        }
     }
 
     public void delete(Long id) {
         var currentUser = userUtils.getCurrentUser();
 
-        if (currentUser == null || !currentUser.getId().equals(id)) {
-            throw new AccessDeniedException("Access denied");
-        } else if (!currentUser.getTasks().isEmpty()) {
+        if (!currentUser.getTasks().isEmpty()) {
             throw new AssociatedWithEntityException("You still have some tasks!");
         }
 
